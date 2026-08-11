77d6a7f92f8a514c8d165f1ad1293076e02888c1
Elaf <mikikon1233@gmail.com>
2026-08-11 08:57:30 +0300

    fix backend issues

--- Controller/authController.js
+++ Controller/authController.js
@@ -3,6 +3,7 @@ const authService = require("../Service/authService");
 const authregister = async (req, res, next) => {
   try {
     await authService.register(req.body);
+    console.log("routes work");
 
     return res.status(200).json({ message: "User created" });
   } catch (error) {
