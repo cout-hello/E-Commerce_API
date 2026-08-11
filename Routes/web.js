b989369e5e9f38a0a62f0d52cecc7a6a8e33d95b
Elaf <mikikon1233@gmail.com>
2026-08-11 07:00:06 +0300

    fix backend issues

--- Routes/web.js
+++ Routes/web.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 
+const authRoutes = require("./authRoutes");
 
-app.use("/user", router);
+app.use(authRoutes);
 router.get("/", (req, res) => {
   return res.status(200).json({
     message: "root path",
--- Service/authService.js
+++ Service/authService.js
@@ -1,28 +1,32 @@
 // userData its (req.body)
 const Jio = require("joi");
 
-const registerSchrema = joi.object({
+const registerSchrema = Jio.object({
   name: Jio.string()
     .min(3)
     .max(10)
     .pattern(/^[A-Za-z]+$/)
     .required(),
-  username: Jio.string().min(3).max(10).required(),
+  username: Jio.string()
+    .pattern(/^[A-Za-z0-9_]+$/)
+    .min(3)
+    .max(10)
+    .required(),
   email: Jio.string().email().required(),
-  phoneNumber: Jio.string().required(),
-  city: Jio.string()
+  phoneNumber: Jio.string()
     .pattern(/^05\d{8}$/)
     .required(),
+  city: Jio.string().required(),
   shippingAddress: Jio.string().required(),
   password: Jio.string().min(8).max(30).required(),
 });
 
 const register = async (userData) => {
-    const{error, value} = registerSchrema.validate(userData);
+  const { error, value } = registerSchrema.validate(userData);
 
-    if(error){
-        throw error;
-    }
+  if (error) {
+    throw error;
+  }
 };
 
 module.exports = register;
