c70b92cf5d68a2c3a6d6dfb37cf9f276594625b9
Elaf <mikikon1233@gmail.com>
2026-08-13 11:57:01 +0300

    fix backend issues

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const { router } = require("./Routes/web");
 const connectDB = require("./config/database");
 const PORT = process.env.PORT;
 
+//hey Expresss, when i use res.render() use EJS to convert template to html
+app.set("view engine", "ejs");
+// hey Express, all EJS files its inside the /views folder 
+app.set("views", "./views");
+
 app.use(express.json());
 app.use("/", router);
+app.get("/products", (req, res) => {
+  res.render("products_list");
+});
 
 const startserver = async () => {
   try {
